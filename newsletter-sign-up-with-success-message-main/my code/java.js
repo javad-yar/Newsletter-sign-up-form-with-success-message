3d9131f539bd7d1f24fe1a39a2a3164f5ae545c0
javad-yar <150520057+javad-yar@users.noreply.github.com>
2024-03-27 00:44:36 +0330

    Update java.js

--- newsletter-sign-up-with-success-message-main/my code/java.js
+++ newsletter-sign-up-with-success-message-main/my code/java.js
@@ -8,19 +8,4 @@ $(document).ready(function () {
         this.style = "border-color: hsl(231, 7%, 60%);";
 
     })
-//     var timer;
-
-//     $("button").hover(mouseEnter, mouseLeave);
-//     $("button").hover
-//         function mouseEnter() {
-//             this.style.delay = "background-color: rgb(255, 83, 141);"
-//             this.style = "background-image: linear-gradient(to right, rgb(255, 83, 141) , rgb(234, 102, 50)); box-shadow: 10px 10px 15px rgba(253, 114, 160, 0.355); transition: background-image 10s ease-out" ;
-
-//         };
-//         function mouseLeave() {
-//             this.style = "background-color: hsl(234, 29%, 20%);"
-
-//         };
-    
-// })
-
+})
