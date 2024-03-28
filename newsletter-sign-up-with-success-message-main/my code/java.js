43d78e4d0d7b3075a29e57eb6e375379985c509b
javad-yar <150520057+javad-yar@users.noreply.github.com>
2024-03-28 18:58:49 +0330

    Update java.js

--- newsletter-sign-up-with-success-message-main/my code/java.js
+++ newsletter-sign-up-with-success-message-main/my code/java.js
@@ -8,4 +8,12 @@ $(document).ready(function () {
         this.style = "border-color: hsl(231, 7%, 60%);";
 
     })
+    function ValidateEmail() {
+        let mail = document.getElementById("inp").value
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)){
+            return (true)
+        }
+        alert("You have entered an invalid email address!")
+        return (false)
+    }
 })
